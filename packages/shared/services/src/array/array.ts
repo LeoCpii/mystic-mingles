f37131d36d41405eb14abc170739ffaf38d5cd8a
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-01-12 15:21:26 -0300

    🎉 (services): begin project

---
+++ packages/shared/services/src/array/array.ts
@@ -0,0 +1,20 @@
+export function shuffle(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+export function chooseNumber(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function getRandom<T>(arr: T[]): T {
+    const shuffled = shuffle(arr);
+    const chosen = chooseNumber(0, shuffled.length - 1);
+
+    return shuffled[chosen];
+}
